dea8b08d9af5cade7070c3eea6aaf302fd776edd
Schwebbe <sebbetheme@hotmail.com@hotmail.com>
2017-09-06 14:30:02 +0200

    Successfull adding of tables!

--- pouchDB-nodeJS/server.js
+++ pouchDB-nodeJS/server.js
@@ -16,6 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+//index route till servern
 app.get('/', function (req, res) {
     if (!error) {
         console.log('Loaded...');
